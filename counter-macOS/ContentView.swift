403f67ab138ee24edbb62a971477374454cea020
Luis Subiabre <luis.subiabre@gmail.com>
2022-01-26 21:27:41 -0300

    first

--- counter-macOS/ContentView.swift
+++ counter-macOS/ContentView.swift
@@ -8,9 +8,57 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var valorPlayer1 = 24
+    @State var valorPlayer2 = 24
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+
+        VStack{
+            Image("logoLS")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 96, height: 96)
+                .clipShape(Circle())
+            HStack {
+                
+                VStack {
+                    Text("Player 1")
+                        .font(.largeTitle)
+                    Text("\(valorPlayer1)")
+                        .padding()
+                        .font(.title2)
+                    HStack {
+                        Button("+") {
+                            self.valorPlayer1 += 1
+                           }
+                        
+                        Button("-") {
+                            self.valorPlayer1 -= 1
+                        }
+                    }
+
+                }
+    Divider()
+                VStack {
+                    Text("Player 2")
+                        .font(.largeTitle)
+                    Text("\(valorPlayer2)")
+                        .padding()
+                        .font(.title2)
+                    HStack {
+                        Button("+") {
+                            self.valorPlayer2 += 1
+                           }
+                        
+                        Button("-") {
+                            self.valorPlayer2 -= 1
+                        }
+                    }
+
+                }
+            }
+        }
+
+       
     }
 }
 
