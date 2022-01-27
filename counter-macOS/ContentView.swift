4f27f10bb3074c214f9ed9fd48d883094cfb7fbc
Luis Subiabre <luis.subiabre@gmail.com>
2022-01-26 21:23:22 -0300

    Initial Commit

---
+++ counter-macOS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  counter-macOS
+//
+//  Created by Luis Subiabre Salviat on 26-01-22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ counter-macOS/counter_macOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  counter_macOSApp.swift
+//  counter-macOS
+//
+//  Created by Luis Subiabre Salviat on 26-01-22.
+//
+
+import SwiftUI
+
+@main
+struct counter_macOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
